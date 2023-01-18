2d2baa075a20b99da2b1dfe534b98a276236f119
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-18 18:55:51 +0100

    Add solution 1ex

--- Project 2/src/components/ProductList.js
+++ Project 2/src/components/ProductList.js
@@ -1,18 +1,18 @@
-import { ProductListItem } from "./ProductListItem"
+import { ProductListItem } from './ProductListItem';
 
 export const ProductList = (props) => {
-
-
-
-
-    return (
-        <div>
-           <h2>Product List</h2>
-           <p>{props.products.map((el) => (
-            <ProductListItem title={el.title} description={el.description} price={el.price} />
-        ))}</p>
-        
-            
-        </div>
-    )
-}
+	return (
+		<div>
+			<h2>Product List</h2>
+			<p>
+				{props.products.map((el) => (
+					<ProductListItem
+						title={el.title}
+						description={el.description}
+						price={el.price}
+					/>
+				))}
+			</p>
+		</div>
+	);
+};
--- Project 2/src/components/ProductListItem.js
+++ Project 2/src/components/ProductListItem.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 
+export const ProductListItem = (props) => {
 
-export const ProductListItem = () => {
+    const [price, setPrice] = useState([])
 
+    const SumPrice = () => {
+        console.log(props.price);
+    }
 
-
-    return (
-
-        <div>
-            Products Item
-        </div>
-    )
-}
+	return (
+		<div>
+			{props.title} {props.description} {props.price}
+            <button onClick={SumPrice}>Dodaj do koszyka</button>
+		</div>
+	);
+};
