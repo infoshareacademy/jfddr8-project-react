266fe6689fb2e69037429a31a6f08fa5dc3f17a8
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-01-19 11:36:51 +0100

    Zadanie 3.4 Done

--- Project 2/src/App.js
+++ Project 2/src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
 import ProductList from "./Components/ProductList";
 import ShoppingCart from "./Components/ShoppingCart";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
+import { LoginStatus } from "./Components/Auth";
 
 function App() {
+  const {isLogged, setIsLogged} = useContext(LoginStatus)
+  
   const [shoppingCart, setShoppingCart] = useState(0);
-  const [isLogged, setIsLogged] = useState(false);
+
 
   const addToShopping = (itemPrice) => {
     setShoppingCart(itemPrice + shoppingCart);
   };
-  // console.log(shoppingCart)
+
 
   return (
     <BrowserRouter>
