df8f915d8e099a6000494c7d34c601f8e8a2d6ef
olga-kacala <olga.kacala@gmail.com>
2023-02-02 14:20:49 +0100

    Exe 6 partially done

--- Project 2/src/App.tsx
+++ Project 2/src/App.tsx
@@ -1,17 +1,31 @@
+// import od Maja
+
 import './App.css';
-import { useState, useContext } from 'react'
+import { useState, useContext , useEffect} from 'react'
 import {
   BrowserRouter,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import { Home } from './components/Home/Home';
+import { Home } from './components/Home';
 import { AuthContext } from './providers/Auth';
-import { Login } from './components/Login/Login';
+import { Login } from './components/Login';
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
-  const {isLogged} = useContext(AuthContext);
+  const {isLogged, setIsLogged} = useContext(AuthContext);
+
+  useEffect(()=>
+{
+
+},[]);
   const [basketValue, setBasketValue] = useState(0);
 
   const addToCart = (productValue: number) => {
--- Project 2/src/components/Login.tsx
+++ Project 2/src/components/Login.tsx
@@ -1,31 +1,96 @@
+
 import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/Auth";
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
 
 
 export const Login = (): JSX.Element => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-
-    const { isLogged, setIsLogged } = useContext(AuthContext);
+    const { setIsLogged } = useContext(AuthContext);
+    const [login, setLogin] = useState<string>('');
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if(isLogged) {
-            navigate('/home');
-        }
-    }, [isLogged, navigate]);
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!username || !password) {
-            alert('Please provide user credentials');
-            return;
-        } else {
-            localStorage.setItem('user', username);
-            setIsLogged(true);
-        }
+    const handleSubmit = async (event:React.FormEvent)=> {
+      event.preventDefault();
+      try {
+        await createUserWithEmailAndPassword(auth, password, login)
+        navigate ('/home')
+      } catch (error) {
+        error? signInWithEmailAndPassword: navigate('/login');
       }
+    
+
+//     const handleLogin = ({email, password}) =>
+//     signInWithEmailAndPassword(auth, email, password);
+
+
+// }
+
+
+//     // useEffect(() => {
+//     //     if(isLogged) {
+//     //         navigate('/home');
+//     //     }
+//     // }, [isLogged, navigate]);
+
+//     // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     //     e.preventDefault();
+//     //     if (!username || !password) {
+//     //         alert('Please provide user credentials');
+//     //         return;
+//     //     } else {
+//     //         localStorage.setItem('user', username);
+//     //         setIsLogged(true);
+//     //     }
+//     //   }
+//     useEffect(() => {
+//       const unsubscribe = onAuthStateChanged(auth, (user) => {
+//         if (user) {
+//           navigate("/");
+//         }
+//         setCurrentUser(user);
+//       });
+  
+//       return () => unsubscribe();
+//     }, [navigate]);
+  
+//     const handleLogin = ({ email, password }) =>
+//       signInWithEmailAndPassword(auth, email, password);
+  
+//     const handleLogOut = () => signOut(auth);
+  
+//     const handleRegister = ({ email, password }) =>
+//       createUserWithEmailAndPassword(auth, email, password);
+  
+//     if (currentUser) {
+//       return (
+//         <AuthenticatedHomePage
+//           currentUser={currentUser}
+//           onLogOut={handleLogOut}
+//         />
+//       );
+//     }
+  
+
+//       const handleSubmit = async (event) => {
+//         event.preventDefault();
+//         try {
+//           await onSubmit({
+//             email: event.target.elements.email.value,
+//             password: event.target.elements.password.value,
+//           });
+//         } catch (error) {
+//           setLoginError(true);
+//         }
+};
 
     return (
         <>
