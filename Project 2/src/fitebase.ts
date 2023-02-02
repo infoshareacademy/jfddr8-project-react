8b1dcd47ee98b39779a9fc119ffeda56b7b86432
Weronika Niekludow <118271719+weronika-niekludow@users.noreply.github.com>
2023-02-02 15:02:39 +0100

    task 5 finished, task 6

---
+++ Project 2/src/fitebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCVkjXkZ6V__xq2MDgW1jFyRN_ngIB2N7Q",
+  authDomain: "projekt2kurs.firebaseapp.com",
+  projectId: "projekt2kurs",
+  storageBucket: "projekt2kurs.appspot.com",
+  messagingSenderId: "95470247923",
+  appId: "1:95470247923:web:06844107e5a7598e40fc86",
+};
---
+++ Project 2/src/utils/fetchItems.test.ts
@@ -0,0 +1,36 @@
+import { fetchItems } from "./fetchItems";
+
+describe("fetchItems", () => {
+  const users = [
+    {
+      id: 1,
+      email: "test@test.com",
+      first_name: "test",
+      last_name: "test",
+      avatar: "avatar",
+    },
+  ];
+  const fetchSpy = jest.spyOn(window, "fetch");
+  it("it returns error of resource is empty", async () => {
+    const response = await fetchItems("jakiś strink");
+    expect(fetchSpy).toBeCalledTimes(0);
+    expect(response).toEqual({ success: false });
+  });
+  it("it returns promise resolve", async () => {
+    fetchSpy.mockImplementationOnce(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ data: users }),
+      } as Response)
+    );
+    const response = await fetchItems("users");
+    expect(fetchSpy).toHaveBeenCalledWith("https://reqres.in/api/users");
+    expect(fetchSpy).toBeCalledTimes(1);
+    expect(response).toEqual({ success: true, resource: users });
+  });
+  it("it returns error", async () => {
+    fetchSpy.mockImplementationOnce(() => Promise.reject(""));
+    const response = await fetchItems("users");
+    expect(response).toEqual({ success: false });
+    expect(fetchSpy).toBeCalledTimes(1);
+  });
+});
