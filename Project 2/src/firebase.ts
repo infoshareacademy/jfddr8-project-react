68e58a27c6c40e2e85144d660d7db198d8e121df
Szymon Chiczewski <szchiczewski@gmail.com>
2023-02-02 19:01:05 +0100

    exercise 6 firebase

---
+++ Project 2/src/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbIewBmpQtXtNnmee43nkcFvbLUaW6VPY",
+  authDomain: "project-2-6915e.firebaseapp.com",
+  projectId: "project-2-6915e",
+  storageBucket: "project-2-6915e.appspot.com",
+  messagingSenderId: "707106620680",
+  appId: "1:707106620680:web:83795c5371a7f2dd3258bd"
+};
