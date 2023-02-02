acfae23f7ee4bd6008a3f9c8f3c98889ae9b8cea
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-02-02 10:29:37 +0100

    Add Cypress test 2e2

--- Project 2/cypress/e2e/Login.cy.ts
+++ Project 2/cypress/e2e/Login.cy.ts
@@ -1,1 +1,22 @@
-describe('')
+import cypress from "cypress"
+
+
+describe('Test logowania do strony', ()=>{
+    let user;
+    before(()=>{
+        cy.fixture("user.json").then((e)=>{user=e})
+    })
+    it('e2e application',()=>{
+        const {login, password} = user
+        cy.visit('/login')
+        cy.get("input[placeholder=\"login\"]").type(login);
+        cy.get("input[placeholder=\"password\"]").type(password);
+        cy.contains("Login").click();
+        cy.url().should('include', '/home');
+        cy.getAllLocalStorage().then((data)=> {expect(data,'Admin')})
+        cy.contains('Log out').click()
+    })
+
+
+
+})
