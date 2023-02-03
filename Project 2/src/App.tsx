625966e1f5aae5f168a1f03a6173697593a974b0
olga-kacala <olga.kacala@gmail.com>
2023-02-03 14:29:50 +0100

    Firebase Shopping Cart done

--- Project 2/src/App.tsx
+++ Project 2/src/App.tsx
@@ -1,59 +1,90 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from "react";
 
-import { ProductsData } from './data/products';
-import './App.css';
-import { Home } from './components/Home';
-import { Login } from './components/Login';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthContext } from './providers/Auth';
+import { ProductsData } from "./data/products";
+import "./App.css";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./providers/Auth";
 import {
-	signInWithEmailAndPassword,
-	signOut,
-	onAuthStateChanged,
-	createUserWithEmailAndPassword,
-  } from "firebase/auth";
-  import { auth } from "./firebase";
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
+import { db } from "./firebase";
+import { addDoc, collection, getDoc, doc, setDoc } from "firebase/firestore";
+import { convertToObject } from "typescript";
 
 function App() {
-	const [price, setPrice] = useState<number>(0);
-	const { isLogged, setIsLogged } = useContext(AuthContext);
+  const [price, setPrice] = useState<number>(0);
+  const { isLogged, setIsLogged } = useContext(AuthContext);
+  const [userEmail, setUserEmail] = useState("");
 
-	useEffect(() => {
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setIsLogged(true);
+        console.log(user.email);
+        setUserEmail(`${user.email}`);
+        const docRef = doc(db, "cardValue", `${user.email}`);
 
-	}, [])
+        const docSnap = await getDoc(docRef);
 
-	const SumPrice = (currentProductPrice: number): void => {
-		setPrice(price + currentProductPrice);
-	};
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          console.log(data);
+          setPrice(data.value);
+        }
+      } else {
+        setIsLogged(false);
+      }
+    });
+  }, [setIsLogged, price]);
 
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/home'
-						element={
-							isLogged ? (
-								<Home
-									products={ProductsData}
-									sumPrice={SumPrice}
-									price={price}
-								/>
-							) : (
-								<Navigate to='/login' />
-							)
-						}></Route>
-					<Route
-						path='/login'
-						element={isLogged ? <Navigate to='/home' /> : <Login />}></Route>
-					<Route
-						path='/'
-						element={isLogged ? <Navigate to='/home' /> : <Login />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  const SumPrice = async (currentProductPrice: number): Promise<void> => {
+    try {
+      setPrice(price + currentProductPrice);
+      await setDoc(doc(db, "cardValue", userEmail), {
+        value: price + currentProductPrice,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              isLogged ? (
+                <Home
+                  products={ProductsData}
+                  sumPrice={SumPrice}
+                  price={price}
+                />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={isLogged ? <Navigate to="/home" /> : <Login />}
+          ></Route>
+          <Route
+            path="/"
+            element={isLogged ? <Navigate to="/home" /> : <Login />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- Project 2/src/components/ShoppingCart.tsx
+++ Project 2/src/components/ShoppingCart.tsx
@@ -1,20 +1,20 @@
-import { AuthContext } from '../providers/Auth';
-import { useContext } from 'react';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
+import { AuthContext } from "../providers/Auth";
+import { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 
 export const ShoppingCart: React.FC<{ price: number }> = (props) => {
-	const { setIsLogged } = useContext(AuthContext);
+  const { setIsLogged } = useContext(AuthContext);
 
-	const logoutHandler = () => {
-		setIsLogged(false);
-		signOut(auth);
-	};
+  const logoutHandler = () => {
+    setIsLogged(false);
+    signOut(auth);
+  };
 
-	return (
-		<div>
-			<h1>Total Product Price: {props.price}</h1>
-			<button onClick={() => logoutHandler()}>Log out</button>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Total Product Price: {props.price}</h1>
+      <button onClick={() => logoutHandler()}>Log out</button>
+    </div>
+  );
 };
