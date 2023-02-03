b73e1e9cc7da68a5bade59d77f60b3317b9ffc65
Jakub Dolecki <jakubdolecki1@wp.pl>
2023-02-03 11:48:22 +0100

    Adds price to database

--- Project 2/src/App.js
+++ Project 2/src/App.js
@@ -1,45 +1,62 @@
-import './App.css';
-import { useState, useContext } from 'react'
+import "./App.css";
+import { useState, useContext, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { AuthContext } from "./providers/Auth";
+import { Login } from "./components/Login/Login";
+import { firebaseAuth } from "./firebase";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { Home } from './components/Home/Home';
-import { AuthContext } from './providers/Auth';
-import { Login } from './components/Login/Login';
+  setPersistence,
+  signInWithRedirect,
+  inMemoryPersistence,
+  GoogleAuthProvider,
+  browserLocalPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { app } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseDb } from "./firebase";
+import { collection } from "firebase/firestore";
+import { doc, addDoc, set } from "firebase/firestore";
 
 function App() {
-  const {isLogged} = useContext(AuthContext);
+  const { isLogged, setIsLogged } = useContext(AuthContext);
   const [basketValue, setBasketValue] = useState(0);
 
   const addToCart = (productValue) => {
-      setBasketValue(basketValue + productValue);
-  }
+    const pricesRef = collection(firebaseDb, "prices");
+    addDoc(pricesRef, {productValue}).then((response) => {
+      console.log(response);
+    });
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (data) => {
+      if (data) {
+        setIsLogged(true);
+      } else {
+        setIsLogged(false);
+      }
+    });
+  }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-            isLogged ? (
-              <Navigate to='/home' />
-            ) : (
-              <Navigate to='/login' />
-            )} 
-        />       
-        <Route path="/home" element={
-          <Home 
-            basketValue={basketValue}
-            addToCart={addToCart}
-          />
-        }/>
-        <Route path="/login" element={
-          <Login />
-        }/>
+        <Route
+          path="/"
+          element={
+            isLogged ? <Navigate to="/home" /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/home"
+          element={<Home basketValue={basketValue} addToCart={addToCart} />}
+        />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
