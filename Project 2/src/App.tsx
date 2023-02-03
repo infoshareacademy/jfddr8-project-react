e8d2c31659dfb298377eafe3f457b78f997b1af3
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-03 14:37:07 +0100

    new README

--- Project 2/src/App.tsx
+++ Project 2/src/App.tsx
@@ -1,21 +1,44 @@
 import './App.css';
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import {
   BrowserRouter,
   Routes,
   Route,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
 import { Home } from './components/Home/Home';
 import { AuthContext } from './providers/Auth';
 import { Login } from './components/Login/Login';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from './firebase';
+import { db } from './firebase'
+import { addDoc, collection } from 'firebase/firestore';
+
 
 function App() {
-  const {isLogged} = useContext(AuthContext);
+  const {isLogged, setIsLogged} = useContext(AuthContext);
   const [basketValue, setBasketValue] = useState(0);
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      console.log(user)
+      if (user) {
+        setIsLogged(true)
+      } else {
+        setIsLogged(false)
+      }
+    });
+  }, [])
 
   const addToCart = (productValue: number) => {
       setBasketValue(basketValue + productValue);
+
+      const cartCollection = collection(db,'basketValue', id, 'userBasket')
+      await addDoc(cartCollection), {
+        basketValue: 
+        userBasket: 
+      }
   }
 
   return (
