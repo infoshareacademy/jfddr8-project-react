bbbc10cdc9355696558482c140e8d7ae901c14d0
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-02-03 14:35:56 +0100

    firebase- task5 done

--- Project 2/src/App.tsx
+++ Project 2/src/App.tsx
@@ -1,36 +1,53 @@
 import "./App.css";
 import { useState, useContext, useEffect } from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./Components/Login";
 import Home from "./Components/Home";
 import { LoginStatus } from "./providers/Auth";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from ".";
+import { firebaseDb } from ".";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 function App() {
   const { isLogged } = useContext(LoginStatus);
   const [shoppingCart, setShoppingCart] = useState<number>(0);
   const { setIsLogged } = useContext(LoginStatus);
+  const [user, setUser] = useState<string>("");
 
-  const addToShopping = (itemPrice: number): void => {
-    setShoppingCart(itemPrice + shoppingCart);
+  const addToShopping = async (itemPrice: number): Promise<void> => {
+    try {
+      await setDoc(doc(firebaseDb, "shoppingCart", user), {
+        shoppingCartValue: itemPrice + shoppingCart,
+      });
+      setShoppingCart(itemPrice + shoppingCart);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    onAuthStateChanged(firebaseAuth, (user) => {
+    onAuthStateChanged(firebaseAuth, async (user) => {
       if (user) {
+        setUser(`${user.email}`);
         setIsLogged(true);
+        try {
+          const cartValueSnapshot = await getDoc(
+            doc(firebaseDb, "shoppingCart", `${user.email}`)
+          );
+          if (cartValueSnapshot.exists()) {
+            const { shoppingCartValue } = cartValueSnapshot.data();
+            setShoppingCart(shoppingCartValue);
+          }
+        } catch (error) {
+          console.log(error);
+        }
       } else {
         setIsLogged(false);
+        setShoppingCart(0);
       }
     });
-  }, [setIsLogged]);
+  }, [setIsLogged, setShoppingCart]);
 
   return (
     <BrowserRouter>
